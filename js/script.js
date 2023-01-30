d6abb55dbc79a9f4aeedd3b86f870da747ae040c
bia <lba0507@naver.com>
2023-01-30 18:15:52 +0900

    color change

--- js/script.js
+++ js/script.js
@@ -25,10 +25,22 @@ $('.section').each(function(index){
         if(wheelDelta<0){
             if(index<$('section').size()-1){
                 $('html, body').stop().animate({scrollTop: $(this).next().offset().top},500)
+                $('.circle').removeClass('active')
+                let _this = $(this)
+                setTimeout(function(){
+                    _this.next().find('.circle').addClass('active')
+                },300)
+                $(this).next().find('.bg').css('width',0).stop().animate({width:'100%'},500)
             }
         }else{
             if(index>0){
                 $('html, body').stop().animate({scrollTop: $(this).prev().offset().top},500)
+                $('.circle').removeClass('active')
+                let _this = $(this)
+                setTimeout(function(){
+                    _this.prev().find('.circle').addClass('active')
+                },300)
+                $(this).find('.bg').css('width','100%').stop().animate({width:'0'},500)
             }
         }
     })
@@ -77,3 +89,42 @@ closeBtn.addEventListener('click',function(){
 contactBtn.addEventListener('click',function(){
     contact.style.display='block'
 });
+
+//section header
+const header = $('#header');
+const section3 = $('#section3')
+const section4 = $('#section4')
+const section5 = $('#section5')
+const section6 = $('#section6')
+$(window).scroll(function(){
+    // if($(document).scrollTop() >= section3.offset().top && $(document).scrollTop() < section4.offset().top){
+    //     header.css('background','#929292')
+    // }
+    // else if($(document).scrollTop() >= section4.offset().top && $(document).scrollTop() < section5.offset().top){
+    //     header.css('background','#1A3F83')
+    // }
+    // else if($(document).scrollTop() >= section5.offset().top && $(document).scrollTop() < section6.offset().top){
+    //     header.css('background','linear-gradient(0deg, rgba(255, 143, 197, 0.78) 0%, rgba(255, 79, 163, 0.78) 64.9%, rgba(255, 143, 197, 0.78) 100%)')
+    // }
+    // else if($(document).scrollTop() >= section6.offset().top){
+    //     header.css('background','rgba(23, 156, 120, 0.84)')
+    // }
+    // else{
+    //     header.css('background','hsl(193, 27%, 31%)')
+    // }
+    if($(document).scrollTop() == section3.offset().top){
+        header.css('background','#929292')
+    }
+    else if($(document).scrollTop() == section4.offset().top){
+        header.css('background','#1A3F83')
+    }
+    else if($(document).scrollTop() == section5.offset().top){
+        header.css('background','linear-gradient(0deg, rgba(255, 143, 197, 0.78) 0%, rgba(255, 79, 163, 0.78) 64.9%, rgba(255, 143, 197, 0.78) 100%)')
+    }
+    else if($(document).scrollTop() == section6.offset().top){
+        header.css('background','rgba(23, 156, 120, 0.84)')
+    }
+    else{
+        header.css('background','hsl(193, 27%, 31%)')
+    }
+})
